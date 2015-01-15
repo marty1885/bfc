f756c91be31ace8110b1244a8cc2d22c470fbd65
Inndy <Inndy.mail@gmail.com>
2015-01-15 11:57:03 +0800

    gg=G

--- bfc.cpp
+++ bfc.cpp
@@ -24,72 +24,72 @@ Options:\n\
 
 int main(int argc,char** argv)
 {
-        bool outputAsm = false;
-        bool outSet = false;
-        if(argc == 1)//no args
-	{
+    bool outputAsm = false;
+    bool outSet = false;
+    if(argc == 1)//no args
+    {
+        cout << helpString;
+        exit(0);
+    }
+
+    string sourcePath;
+    string outputPath;
+
+    for(int i=1;i<argc;i++)
+    {
+        if(argv[i][0] == '-')
+        {
+            if(argv[i][1] == 'o')
+            {
+                outputPath = argv[i+1];
+                outSet = true;
+                i++;
+            }
+            if(argv[i][1] == 'h')
+            {
                 cout << helpString;
-		exit(0);
-	}
-
-        string sourcePath;
-        string outputPath;
-
-        for(int i=1;i<argc;i++)
-        {
-                if(argv[i][0] == '-')
-                {
-                        if(argv[i][1] == 'o')
-                        {
-                                outputPath = argv[i+1];
-                                outSet = true;
-                                i++;
-                        }
-                        if(argv[i][1] == 'h')
-                        {
-                                cout << helpString;
-                                exit(0);
-                        }
-                        if(argv[i][1] == 'S')
-                                outputAsm = true;
-                }
-                else
-                        sourcePath = argv[i];
-        }
-        if(sourcePath.compare("") == 0)//if no source
-        {
-                cout << "no source file setted. exit\n";
                 exit(0);
-        }
-        if(outSet == false)
-        {
-                outputPath = sourcePath + ".o";
-        }
-
-
-        ifstream in(sourcePath.c_str());
-        string sourceCode((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
-
-        string asmCode = bfCompile(sourceCode.c_str());
-
-        if(outputAsm == false)
-        {
-                ofstream asmOut("cache.asm");
-                asmOut << asmCode;
-                asmOut.close();
-                char* command = new char[4000];
-                sprintf(command,"nasm -f elf64 cache.asm -o cache.o && gcc ./cache.o -o %s",
-                outputPath.c_str());
-
-                int success = system(command);
-                remove("./cache.asm");
-                return success;
+            }
+            if(argv[i][1] == 'S')
+                outputAsm = true;
         }
         else
-        {
-                ofstream asmOut(outputPath.c_str());
-                asmOut << asmCode;
-                asmOut.close();
-        }
-        return 1;
+            sourcePath = argv[i];
+    }
+    if(sourcePath.compare("") == 0)//if no source
+    {
+        cout << "no source file setted. exit\n";
+        exit(0);
+    }
+    if(outSet == false)
+    {
+        outputPath = sourcePath + ".o";
+    }
+
+
+    ifstream in(sourcePath.c_str());
+    string sourceCode((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+
+    string asmCode = bfCompile(sourceCode.c_str());
+
+    if(outputAsm == false)
+    {
+        ofstream asmOut("cache.asm");
+        asmOut << asmCode;
+        asmOut.close();
+        char* command = new char[4000];
+        sprintf(command,"nasm -f elf64 cache.asm -o cache.o && gcc ./cache.o -o %s",
+                outputPath.c_str());
+
+        int success = system(command);
+        remove("./cache.asm");
+        return success;
+    }
+    else
+    {
+        ofstream asmOut(outputPath.c_str());
+        asmOut << asmCode;
+        asmOut.close();
+    }
+    return 1;
 }
--- libbfc.cpp
+++ libbfc.cpp
@@ -30,158 +30,158 @@ mov rbx,rcx ;store current data pointer in rbx\n\
 
 static char BFAsmEnd [] =
 "\n\
-;program end\n\
-;release memory\n\
-mov rdi,rcx\n\
-call free\n\
-mov edi,0\n\
-call exit\n\
-\n\
-";
+        ;program end\n\
+        ;release memory\n\
+        mov rdi,rcx\n\
+        call free\n\
+        mov edi,0\n\
+        call exit\n\
+        \n\
+        ";
 
 extern "C"
 {
 
-const char* bfCompileFrontBracket(int id)
-{
+    const char* bfCompileFrontBracket(int id)
+    {
         static char* str = NULL;
         if(str != NULL)
-                delete [] str;
+            delete [] str;
         str = new char [100];
         sprintf(str,"\
-cmp BYTE[rbx],0\n\
-jz JUPM_END_FLAG_%d\n\
-JUPM_FRONT_FLAG_%d:\n"
-,id,id);
+                cmp BYTE[rbx],0\n\
+                jz JUPM_END_FLAG_%d\n\
+                JUPM_FRONT_FLAG_%d:\n"
+                ,id,id);
         return (const char*)str;
-}
+    }
 
-const char* bfCompileEndBracket(int id)
-{
+    const char* bfCompileEndBracket(int id)
+    {
         static char* str = NULL;
         if(str != NULL)
-                delete [] str;
+            delete [] str;
         str = new char [100];
         sprintf(str,"\
-JUPM_END_FLAG_%d:\n\
-cmp BYTE[rbx],0\n\
-jnz JUPM_FRONT_FLAG_%d\n\
-"
-        ,id,id);
+                JUPM_END_FLAG_%d:\n\
+                cmp BYTE[rbx],0\n\
+                jnz JUPM_FRONT_FLAG_%d\n\
+                "
+                ,id,id);
         return (const char*)str;
-}
+    }
 
-const char* bfCompilePutchar()
-{
+    const char* bfCompilePutchar()
+    {
         static char* str = NULL;
         if(str != NULL)
-                delete [] str;
+            delete [] str;
         str = new char [100];
         sprintf(str,"\
-mov di,[rbx]\n\
-push rcx\n\
-push rbx\n\
-call putchar\n\
-pop rbx\n\
-pop rcx\n\
-");
+                mov di,[rbx]\n\
+                push rcx\n\
+                push rbx\n\
+                call putchar\n\
+                pop rbx\n\
+                pop rcx\n\
+                ");
         return (const char*)str;
-}
+    }
 
-const char* bfCompileGetchar()
-{
+    const char* bfCompileGetchar()
+    {
         static char* str = NULL;
         if(str != NULL)
-                delete [] str;
+            delete [] str;
         str = new char [100];
         sprintf(str,"\
-xor rax,rax\n\
-call getchar\n\
-mov BYTE[rbx],al\n\
-");
+                xor rax,rax\n\
+                call getchar\n\
+                mov BYTE[rbx],al\n\
+                ");
         return (const char*)str;
-}
+    }
 
-int bfCompileCode(string& asmCode,char ch,int length)
-{
+    int bfCompileCode(string& asmCode,char ch,int length)
+    {
         char str[30] = {0};
         static int jumpFlagCount = 0;
         static vector<int> jumpFlagId;
         int id = 0;
         switch(ch)
         {
-                case '+':
+            case '+':
                 if(length == 1)
-                        sprintf(str,"inc BYTE[rbx]\n");
+                    sprintf(str,"inc BYTE[rbx]\n");
                 else
-                        sprintf(str,"add BYTE[rbx],%d\n",length);
+                    sprintf(str,"add BYTE[rbx],%d\n",length);
                 asmCode.append(str);
                 break;
 
-                case '-':
+            case '-':
                 if(length == 1)
-                        sprintf(str,"dec BYTE[rbx]\n");
+                    sprintf(str,"dec BYTE[rbx]\n");
                 else
-                        sprintf(str,"sub BYTE[rbx],%d\n",length);
+                    sprintf(str,"sub BYTE[rbx],%d\n",length);
                 asmCode.append(str);
                 break;
 
-                case '[':
+            case '[':
                 for(int w=0;w<length;w++)
                 {
-                        jumpFlagId.push_back(jumpFlagCount);
-                        asmCode.append(bfCompileFrontBracket(jumpFlagCount));
-                        jumpFlagCount++;
+                    jumpFlagId.push_back(jumpFlagCount);
+                    asmCode.append(bfCompileFrontBracket(jumpFlagCount));
+                    jumpFlagCount++;
                 }
                 break;
 
-                case ']':
+            case ']':
                 for(int w=0;w<length;w++)
                 {
-                        id = jumpFlagId[jumpFlagId.size()-1];
-                        jumpFlagId.pop_back();
-                        asmCode.append(bfCompileEndBracket(id));
+                    id = jumpFlagId[jumpFlagId.size()-1];
+                    jumpFlagId.pop_back();
+                    asmCode.append(bfCompileEndBracket(id));
                 }
                 break;
 
-                case '>':
+            case '>':
                 if(length == 1)
-                        sprintf(str,"inc rbx\n");
+                    sprintf(str,"inc rbx\n");
                 else
-                        sprintf(str,"add rbx,%d\n",length);
+                    sprintf(str,"add rbx,%d\n",length);
                 asmCode.append(str);
                 break;
 
-                case '<':
+            case '<':
                 if(length == 1)
-                        sprintf(str,"dec rbx\n");
+                    sprintf(str,"dec rbx\n");
                 else
-                        sprintf(str,"sub rbx,%d\n",length);
+                    sprintf(str,"sub rbx,%d\n",length);
                 asmCode.append(str);
                 break;
 
-                case '.':
+            case '.':
                 for(int w=0;w<length;w++)
-                        asmCode.append(bfCompilePutchar());
+                    asmCode.append(bfCompilePutchar());
                 break;
 
-                case ',':
+            case ',':
                 for(int w=0;w<length;w++)
-                        asmCode.append(bfCompileGetchar());
+                    asmCode.append(bfCompileGetchar());
                 break;
 
 
         }
         return 1;
-}
+    }
 
-const char* bfCompile(const char* src)
-{
+    const char* bfCompile(const char* src)
+    {
         string sourceCode = src;
         sourceCode.erase(std::remove(sourceCode.begin(), sourceCode.end(), '\n'),
-        sourceCode.end());//remove \n from raw input
+                sourceCode.end());//remove \n from raw input
         sourceCode.erase(std::remove(sourceCode.begin(), sourceCode.end(), ' '),
-        sourceCode.end());//remove ' ' from raw input
+                sourceCode.end());//remove ' ' from raw input
         sourceCode.append(" ");//so the complier will compiler the last chunk of code
 
         string asmCode;
@@ -191,30 +191,30 @@ const char* bfCompile(const char* src)
         char ch = codeStr[0];
         for(int i=1;i<size;i++)
         {
-                //seprate code into chunks
-                int length = size-i;
-                for(int w=0;i<size;i++,w++)
+            //seprate code into chunks
+            int length = size-i;
+            for(int w=0;i<size;i++,w++)
+            {
+                if(ch != codeStr[i])
                 {
-                        if(ch != codeStr[i])
-                        {
-                                length = w+1;
-                                break;
-                        }
-
+                    length = w+1;
+                    break;
                 }
-                //convert to asm
-                bfCompileCode(asmCode,ch,length);
 
-                ch = codeStr[i];
-                //cout << length << " ";
+            }
+            //convert to asm
+            bfCompileCode(asmCode,ch,length);
+
+            ch = codeStr[i];
+            //cout << length << " ";
         }
 
         static string *code = NULL;
         if(code != NULL)
-                delete code;
+            delete code;
         code = new string;
         *code = string(BFAsmHead) + asmCode + string(BFAsmEnd);
         return code->c_str();
-}
+    }
 
 }
